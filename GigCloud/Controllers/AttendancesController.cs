63527f21bb065b419e44fa3f8cc9a5a1cf06e3bf
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-28 15:51:34 +0200

    Created Attendances API

---
+++ GigCloud/Controllers/AttendancesController.cs
@@ -0,0 +1,33 @@
+﻿using GigCloud.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+
+namespace GigCloud.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
