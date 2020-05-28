f1b82205e79f02bc712f0de84d59fbd39107e595
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-28 17:26:46 +0200

    Added "Wh I am following" use case

--- GigCloud/Controllers/FollowingsController.cs
+++ GigCloud/Controllers/FollowingsController.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 
 namespace GigCloud.Controllers
 {
+    [Authorize]
     public class FollowingsController : ApiController
     {
         private ApplicationDbContext _context;
