f0c93bb0217874260b13ee40c5134922e7282211
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-28 16:00:56 +0200

    Finalized the "Attend gigs" use-case by introducing Dtos

--- GigCloud/Controllers/AttendancesController.cs
+++ GigCloud/Controllers/AttendancesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Http;
+using GigCloud.Dtos;
 
 namespace GigCloud.Controllers
 {
@@ -14,18 +15,17 @@ namespace GigCloud.Controllers
         {
             _context = new ApplicationDbContext();
         }
-
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int gigId)
+        public IHttpActionResult Attend(AttendanceDto dto)
         {
             var userId = User.Identity.GetUserId();
 
-            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
                 return BadRequest("The attendance already exists.");
 
             var attendance = new Attendance
             {
-                GigId = gigId,
+                GigId = dto.GigId,
                 AttendeeId = userId
             };
             _context.Attendances.Add(attendance);
---
+++ GigCloud/Dtos/AttendanceDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GigCloud.Dtos
+{
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
