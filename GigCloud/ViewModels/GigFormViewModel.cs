0bd99c85a17e4200c83737091451aa26ac4b2741
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-28 13:22:30 +0200

    Added AddGig controller, view, model and viewmodel

---
+++ GigCloud/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GigCloud.Models;
+using System.Collections.Generic;
+
+namespace GigCloud.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
