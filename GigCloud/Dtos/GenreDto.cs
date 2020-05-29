a68c369a09739b04092d97472d9a54a20c05babc
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-29 23:19:34 +0200

    Added Dtos to return from Web API

---
+++ GigCloud/Dtos/GenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GigCloud.Dtos
+{
+    public class GenreDto
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GigCloud/Dtos/NotificationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigCloud.Models;
+
+namespace GigCloud.Dtos
+{
+    public class NotificationDto
+    {
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        public DateTime? OriginalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+        public GigDto Gig { get; set; }
+    }
+}
