53ba800c6572b0b8ef3efc03f814537d74381dd1
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-31 00:06:55 +0200

    Implemented module dependencies

--- GigCloud/Scripts/app/app.js
+++ GigCloud/Scripts/app/app.js
@@ -1,46 +1,55 @@
-﻿var GigsController = function() {
-    var button;
-
-    var init = function() {
-        $(".js-toggle-attendance").click(toggleAttendance);
-    };
-
-    var toggleAttendance = function(e) {
-                var button = $(e.target);
-                if (button.hasClass("btn-default")) 
-                    createAttendance();
-                 else 
-                    deleteAttendance();
-                
-
-    };
-
-    var createAttendance = function() {
-        $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+﻿var AttendanceService = function () {
+    var createAttendance = function (gigId, done, fail) {
+        $.post("/api/attendances", { gigId: gigId })
             .done(done)
             .fail(fail);
     };
 
-    var deleteAttendance = function() {
+    var deleteAttendance = function (gigId, done, fail) {
         $.ajax({
-                url: "/api/attendances/" + button.attr("data-gig-id"),
+                url: "/api/attendances/" + gigId,
                 method: "DELETE"
             })
             .done(done)
             .fail(fail);
-    }
+    };
 
-    var done = function() {
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    }
+}();
+
+var GigsController = function (attendanceService) {
+    var button;
+
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-attendance", toggleAttendance);
+    };
+
+    var toggleAttendance = function (e) {
+        button = $(e.target);
+
+        var gigId = button.attr("data-gig-id");
+
+        if (button.hasClass("btn-default"))
+            attendanceService.createAttendance(gigId, done, fail);
+        else
+            attendanceService.deleteAttendance(gigId, done, fail);
+    };
+
+    var done = function () {
         var text = (button.text() == "Going") ? "Going?" : "Going";
 
         button.toggleClass("btn-info").toggleClass("btn-default").text(text);
     };
 
-    var fail = function() {
+    var fail = function () {
         alert("Something failed");
     };
 
     return {
         init: init
     }
-}();
+
+}(AttendanceService);
