ce5275b1df3fe6f30b47d611ff1d12d10a83f1c6
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-30 13:02:45 +0200

    Deleted attandance on toggle going button

--- GigCloud/Controllers/Api/AttendancesController.cs
+++ GigCloud/Controllers/Api/AttendancesController.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
-using System.Web.Http;
-using GigCloud.Dtos;
+﻿using GigCloud.Dtos;
 using GigCloud.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigCloud.Controllers.Api
 {
@@ -32,6 +32,24 @@ namespace GigCloud.Controllers.Api
             _context.SaveChanges();
 
             return Ok();
+        }
+
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = _context.Attendances
+                .SingleOrDefault(a => a.AttendeeId == userId && a.GigId == id);
+
+            if (attendance == null)
+                return NotFound();
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
