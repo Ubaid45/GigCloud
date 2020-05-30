c7b23b3e962883003fba028e421419e9b1178021
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-30 12:42:30 +0200

    Implemented Look ups and rendered going button in right state

--- GigCloud/ViewModels/GigsViewModel.cs
+++ GigCloud/ViewModels/GigsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GigCloud.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GigCloud.ViewModels
 {
@@ -9,5 +10,6 @@ namespace GigCloud.ViewModels
         public bool ShowActions { get; set; }
         public string Heading { get; set; }
         public string SearchTerm { get; set; }
+        public ILookup<int, Attendance> Attendances { get; set; }
     }
 }
