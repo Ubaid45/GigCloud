0c91ca63f6c56f5cca209097e700d959296ea997
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-30 23:52:53 +0200

    Implemented DRY principle

--- GigCloud/Scripts/app/app.js
+++ GigCloud/Scripts/app/app.js
@@ -1,48 +1,36 @@
 ﻿var GigsController = function() {
+    var button;
 
     var init = function() {
-        $(".js-toggle-attendance").click(function(e) {
-            var button = $(e.target);
-            if (button.hasClass("btn-default")) {
-                $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                    .done(function() {
-                        button
-                            .removeClass("btn-default")
-                            .addClass("btn-info")
-                            .text("Going");
-                    })
-                    .fail(function() {
-                        alert("Something failed!");
-                    });
-            } else {
-                $.ajax({
-                        url: "/api/attendances/" + button.attr("data-gig-id"),
-                        method: "DELETE"
-                    })
-                    .done(function() {
-                        button
-                            .removeClass("btn-info")
-                            .addClass("btn-default")
-                            .text("Going?");
-                    })
-                    .fail(function() {
-                        alert("Something failed");
-                    });
-            }
+        $(".js-toggle-attendance").click(toggleAttendance);
+    };
 
-        });
+    var toggleAttendance = function(e) {
+                var button = $(e.target);
+                if (button.hasClass("btn-default")) {
+                    $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+                        .done(done)
+                        .fail(fail);
+                } else {
+                    $.ajax({
+                            url: "/api/attendances/" + button.attr("data-gig-id"),
+                            method: "DELETE"
+                        })
+                        .done(done)
+                        .fail(fail);
+                }
 
-        $(".js-toggle-follow").click(function(e) {
-            var button = $(e.target);
-            $.post("/api/followings", { followeeId: button.attr("data-user-id") })
-                .done(function() {
-                    button.text("Following");
-                })
-                .fail(function() {
-                    alert("Something failed");
-                });
-        });
-    }
+    };
+
+    var done = function() {
+        var text = (button.text() == "Going") ? "Going?" : "Going";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function() {
+        alert("Something failed");
+    };
 
     return {
         init: init
