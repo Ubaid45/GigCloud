6002e12775b670ae03395b2a160d355931992738
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-30 23:55:44 +0200

    Better separation of concerns

--- GigCloud/Scripts/app/app.js
+++ GigCloud/Scripts/app/app.js
@@ -7,20 +7,28 @@
 
     var toggleAttendance = function(e) {
                 var button = $(e.target);
-                if (button.hasClass("btn-default")) {
-                    $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
-                        .done(done)
-                        .fail(fail);
-                } else {
-                    $.ajax({
-                            url: "/api/attendances/" + button.attr("data-gig-id"),
-                            method: "DELETE"
-                        })
-                        .done(done)
-                        .fail(fail);
-                }
+                if (button.hasClass("btn-default")) 
+                    createAttendance();
+                 else 
+                    deleteAttendance();
+                
 
     };
+
+    var createAttendance = function() {
+        $.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+            .done(done)
+            .fail(fail);
+    };
+
+    var deleteAttendance = function() {
+        $.ajax({
+                url: "/api/attendances/" + button.attr("data-gig-id"),
+                method: "DELETE"
+            })
+            .done(done)
+            .fail(fail);
+    }
 
     var done = function() {
         var text = (button.text() == "Going") ? "Going?" : "Going";
