4e9a3f3c5bb0e985f174e819089a12dfdc1d5a15
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 00:36:04 +0200

    Implemented Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()

--- GigCloud.Tests/Controllers/Api/GigsControllerTests.cs
+++ GigCloud.Tests/Controllers/Api/GigsControllerTests.cs
@@ -1,8 +1,11 @@
-﻿using GigCloud.Controllers.Api;
+﻿using FluentAssertions;
+using GigCloud.Controllers.Api;
 using GigCloud.Core;
+using GigCloud.Core.IRepositories;
 using GigCloud.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Web.Http.Results;
 
 namespace GigCloud.Tests.Controllers.Api
 {
@@ -10,14 +13,25 @@ namespace GigCloud.Tests.Controllers.Api
     public class GigsControllerTests
     {
         private GigsController _controller;
+        private Mock<IGigRepository> _mockRepository;
 
         public GigsControllerTests()
         {
+            _mockRepository = new Mock<IGigRepository>();
 
             var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Gigs).Returns(_mockRepository.Object);
 
             _controller = new GigsController(mockUoW.Object);
             _controller.MockCurrentUser("1", "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.Cancel(1);
+
+            result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
