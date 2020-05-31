8ef686e8b185f2c5ce131d3812d460b184466bc3
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 00:58:01 +0200

    Initialized Test for Attendance controller

---
+++ GigCloud.Tests/Controllers/Api/AttendancesControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using GigCloud.Controllers.Api;
+using GigCloud.Core;
+using GigCloud.Core.IRepositories;
+using GigCloud.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigCloud.Tests.Controllers.Api
+{
+    public class AttendancesControllerTests
+    {
+        private AttendancesController _controller;
+        private Mock<IAttendanceRepository> _mockRepository;
+        private string _userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IAttendanceRepository>();
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Attendances).Returns(_mockRepository.Object);
+
+            _controller = new AttendancesController(mockUoW.Object);
+            _userId = "1";
+            _controller.MockCurrentUser(_userId, "user1@domain.com");
+        }
+    }
+}
