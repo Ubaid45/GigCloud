4c8c6f614ea0c59aab2fbd12bd973ad5431706cb
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-31 17:32:02 +0200

    Added Ninject to decouple controllers and binded it with default interface

--- GigCloud/Controllers/GigsController.cs
+++ GigCloud/Controllers/GigsController.cs
@@ -11,9 +11,9 @@ namespace GigCloud.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
 
-        public GigsController()
+        public GigsController(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = new UnitOfWork(new ApplicationDbContext());
+            _unitOfWork = unitOfWork;
         }
 
         public ActionResult Details(int id)
