7c7807e8e4b7eda74368661f188e6b5a441b4ad2
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 09:51:54 +0200

    Seed data so that our db is always in cleaned state

--- GigCloud.IntegrationTests/GlobalSetUp.cs
+++ GigCloud.IntegrationTests/GlobalSetUp.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using GigCloud.Core.Models;
+using GigCloud.Persistence;
+using NUnit.Framework;
 using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace GigCloud.IntegrationTests
 {
@@ -10,6 +13,7 @@ namespace GigCloud.IntegrationTests
         public void SetUp()
         {
             MigrateDbToLatestVersion();
+            Seed();
         }
 
         private static void MigrateDbToLatestVersion()
@@ -17,6 +21,19 @@ namespace GigCloud.IntegrationTests
             var configuration = new Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+            context.SaveChanges();
         }
 
     }
