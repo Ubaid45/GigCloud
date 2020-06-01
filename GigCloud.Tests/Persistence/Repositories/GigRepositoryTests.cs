b44f02c113a64b2184cb128bead9fbc24db00d01
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 08:45:58 +0200

    Added "GetUpcomingGigsByArtist_GigIsCanceled_ShouldNotBeReturned" test

--- GigCloud.Tests/Persistence/Repositories/GigRepositoryTests.cs
+++ GigCloud.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -15,14 +15,17 @@ namespace GigCloud.Tests.Persistence.Repositories
     {
         private GigRepository _repository;
         private Mock<DbSet<Gig>> _mockGigs;
+        private Mock<DbSet<Attendance>> _mockAttendances;
 
-        [TestMethod]
+        [TestInitialize]
         public void TestInitialize()
         {
             _mockGigs = new Mock<DbSet<Gig>>();
+            _mockAttendances = new Mock<DbSet<Attendance>>();
 
             var mockContext = new Mock<IApplicationDbContext>();
             mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+            mockContext.SetupGet(c => c.Attendances).Returns(_mockAttendances.Object);
 
             _repository = new GigRepository(mockContext.Object);
         }
@@ -38,5 +41,20 @@ namespace GigCloud.Tests.Persistence.Repositories
 
             gigs.Should().BeEmpty();
         }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsCanceled_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(1), ArtistId = "1" };
+            gig.Cancel();
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUpcomingGigsByArtist("1");
+
+            gigs.Should().BeEmpty();
+
+        }
+
     }
 }
