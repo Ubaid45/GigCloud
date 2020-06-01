56e7eb6b88d3ba2975353a7e60d9ae8af5e899a3
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 09:47:21 +0200

    Setup the integration database

---
+++ GigCloud.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace GigCloud.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetUp
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            MigrateDbToLatestVersion();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
+            var configuration = new Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+
+    }
+}
