4099d1e07c3223311a7a5f99f6bc9dae4ee37249
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 09:07:31 +0200

    Setup TestInitialize for Notification repository

---
+++ GigCloud.Tests/Persistence/Repositories/NotificationRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using GigCloud.Core.Models;
+using GigCloud.Persistence;
+using GigCloud.Persistence.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+
+namespace GigCloud.Tests.Persistence.Repositories
+{
+    [TestClass]
+    public class NotificationRepositoryTests
+    {
+        private NotificationRepository _repository;
+        private Mock<DbSet<UserNotification>> _mockNotifications;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockNotifications = new Mock<DbSet<UserNotification>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.UserNotifications).Returns(_mockNotifications.Object);
+
+
+            _repository = new NotificationRepository(mockContext.Object);
+        }
+    }
+}
