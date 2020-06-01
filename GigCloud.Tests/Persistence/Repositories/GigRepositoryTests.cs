89987c5af4a358aabdd452fc6a0db552deea83b8
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 08:30:30 +0200

    Added "GetUpcomingGigsByArtist_GigIsInThePast_ShouldNotBeReturned" test

--- GigCloud.Tests/Persistence/Repositories/GigRepositoryTests.cs
+++ GigCloud.Tests/Persistence/Repositories/GigRepositoryTests.cs
@@ -1,8 +1,11 @@
-﻿using GigCloud.Core.Models;
+﻿using FluentAssertions;
+using GigCloud.Core.Models;
 using GigCloud.Persistence;
 using GigCloud.Persistence.Repositories;
+using GigCloud.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Data.Entity;
 
 namespace GigCloud.Tests.Persistence.Repositories
@@ -11,13 +14,29 @@ namespace GigCloud.Tests.Persistence.Repositories
     public class GigRepositoryTests
     {
         private GigRepository _repository;
+        private Mock<DbSet<Gig>> _mockGigs;
+
         [TestMethod]
         public void TestInitialize()
         {
-            var mockGigs = new Mock<DbSet<Gig>>();
+            _mockGigs = new Mock<DbSet<Gig>>();
+
             var mockContext = new Mock<IApplicationDbContext>();
-            //mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+            mockContext.SetupGet(c => c.Gigs).Returns(_mockGigs.Object);
+
             _repository = new GigRepository(mockContext.Object);
+        }
+
+        [TestMethod]
+        public void GetUpcomingGigsByArtist_GigIsInThePast_ShouldNotBeReturned()
+        {
+            var gig = new Gig() { DateTime = DateTime.Now.AddDays(-1), ArtistId = "1" };
+
+            _mockGigs.SetSource(new[] { gig });
+
+            var gigs = _repository.GetUpcomingGigsByArtist("1");
+
+            gigs.Should().BeEmpty();
         }
     }
 }
