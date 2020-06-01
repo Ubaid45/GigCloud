d0e09082da3586eff0efa694d6278ef23ac637ce
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 09:19:45 +0200

    Added "Cancel_WhenCalled_ShouldSetIsCanceledToTrue" test

---
+++ GigCloud.Tests/Domain/Models/UnitTest2.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using GigCloud.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace GigCloud.Tests.Domain.Models
+{
+    [TestClass]
+    public class GigTests
+    {
+        // A different approach to write these tests is using BDD. 
+        // 
+        // GivenAnArtistHasAGig
+        //      WhenHeCancelsTheGig
+        //          IsCanceledShouldBeSetToTrue
+        //          EachAttendeeShouldHaveANotification
+        //
+        // 
+        [TestMethod]
+        public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
+        {
+            var gig = new Gig();
+
+            gig.Cancel();
+
+            gig.IsCanceled.Should().BeTrue();
+        }
+
+       
+    }
+}
