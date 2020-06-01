2f9a484501d7ac486cd656a0ff858a08423eb63b
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 10:07:07 +0200

    Initial Setup

---
+++ GigCloud.IntegrationTests/Controllers/GigsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using GigCloud.Controllers;
+using GigCloud.Persistence;
+using NUnit.Framework;
+
+namespace GigCloud.IntegrationTests.Controllers
+{
+    [TestFixture]
+    public class GigsControllerTests
+    {
+        private GigsController _controller;
+        private ApplicationDbContext _context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _context = new ApplicationDbContext();
+            _controller = new GigsController(new UnitOfWork(_context));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+    }
+}
