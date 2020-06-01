070c19101990fafc35e296cfc153de3cf7333026
Ubaid Rana <ubaidrana45@gmail.com>
2020-06-01 11:53:39 +0200

    Fixed "Make sure that the controller has a parameterless public constructor" issue

--- GigCloud/App_Start/NinjectWebCommon.cs
+++ GigCloud/App_Start/NinjectWebCommon.cs
@@ -1,18 +1,18 @@
 using Ninject.Web.Common.WebHost;
+using System.Web.Http;
+using WebApiContrib.IoC.Ninject;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(GigCloud.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(GigCloud.App_Start.NinjectWebCommon), "Stop")]
 
 namespace GigCloud.App_Start
 {
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Extensions.Conventions;
+    using Ninject.Web.Common;
     using System;
     using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-    using Ninject.Extensions.Conventions;
 
     public static class NinjectWebCommon
     {
@@ -50,12 +50,15 @@ namespace GigCloud.App_Start
 
                 RegisterServices(kernel);
 
+
                 kernel.Bind(x =>
                 {
                     x.FromThisAssembly()
                         .SelectAllClasses()
                         .BindDefaultInterface();
                 });
+
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectResolver(kernel);
                 return kernel;
             }
             catch
